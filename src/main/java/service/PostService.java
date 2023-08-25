240067eda9cca0f4a38b31f37ef2c9d3628fc048
Training JAVA_programming <graf1324@mail.ru>
2023-08-25 17:15:13 +0700

    first commit

---
+++ src/main/java/service/PostService.java
@@ -0,0 +1,36 @@
+package service;
+
+import org.springframework.stereotype.Service;
+import exception.NotFoundException;
+import model.Post;
+import repository.IPostRepository;
+import java.util.List;
+
+@Service
+public class PostService {
+    private final IPostRepository repository;
+
+    public PostService(IPostRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Post> all() {
+
+        return repository.all();
+    }
+
+    public Post getById(long id) {
+
+        return repository.getById(id).orElseThrow(NotFoundException::new);
+    }
+
+    public Post save(Post post) {
+
+        return repository.save(post);
+    }
+
+    public void removeById(long id) {
+
+        repository.removeById(id);
+    }
+}
